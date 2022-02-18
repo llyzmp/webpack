5c8a0c3fd8ef54f4d34154eea828aaeb6f07adad
李留洋 <liliuyang@zhejianglab.com>
2022-02-18 11:39:39 +0800

    feat: webpack简单依赖及自定义配置文件名

---
+++ 2.webpack5/03.webpack_dependency_graph/src/index.js
@@ -0,0 +1,7 @@
+import { sum, square } from './js/utils.js'
+const getInfo = require('./js/api.js')
+import './js/test'
+
+console.log(sum(10, 20))
+console.log(square(50, 20))
+console.log(getInfo());
