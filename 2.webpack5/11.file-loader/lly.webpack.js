0d04a78027693609c0bfd5a873d98288ff57ca9b
李留洋 <liliuyang@zhejianglab.com>
2022-02-24 10:58:56 +0800

    feat: file-loader

---
+++ 2.webpack5/11.file-loader/lly.webpack.js
@@ -0,0 +1,48 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'build.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              esModule: false
+            }
+          },
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'less-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|gif|jpe?g)$/,
+        // use: [
+        //   {
+        //     loader: 'file-loader',
+        //     options: {
+        //       esModule: false // 不转为 esModule
+        //     }
+        //   }
+        // ]
+        use: ['file-loader']
+      }
+    ]
+  }
+}
