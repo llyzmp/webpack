1a3f1abe5a87f050c4b881f94c1d65fd7968b75f
李留洋 <liliuyang@zhejianglab.com>
2022-02-28 17:17:00 +0800

    feat: copy-webpack-plugin

---
+++ 2.webpack5/17.copy-webpack-plugin/src/index.js
@@ -0,0 +1,2 @@
+import './js/font'
+import './js/Image'
---
+++ 2.webpack5/17.copy-webpack-plugin/src/js/font.js
@@ -0,0 +1,13 @@
+import '../font/iconfont.css'
+import '../css/index.css'
+
+function packFont() {
+  const oEle = document.createElement('div');
+  const oSpan = document.createElement('span');
+  oSpan.className = 'iconfont icon-linggan lly-icon'
+  oEle.appendChild(oSpan);
+
+  return oEle;
+}
+
+document.body.appendChild(packFont());
