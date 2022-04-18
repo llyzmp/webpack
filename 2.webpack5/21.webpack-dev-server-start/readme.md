f28aef1b312b7b879bd3197b905631046b5b4e85
zmplly <1776321381@qq.com>
2022-04-18 23:37:08 +0800

    feat: webpack-dev-server

---
+++ 2.webpack5/21.webpack-dev-server-start/readme.md
@@ -0,0 +1,14 @@
+# webpack-dev-server
+
+
+- 开发模式
+  - watch
+  - live server
+  - 缺点： 
+    - 1. 所有源码都会重新编
+    - 2. 每次编译成功之后都需要进行文件读写
+    - 3. live server
+    - 4. 不能实现局部刷新
+    - 
+
+webpack-dev-server最合适
---
+++ 2.webpack5/21.webpack-dev-server-start/src/index.js
@@ -0,0 +1,20 @@
+import "core-js/stable"
+import "regenerator-runtime/runtime"
+
+
+
+const title ='虚心若愚';
+const foo = () => {
+  console.log(title);
+}
+
+const p1 = new Promise((resolve, reject) => {
+  console.log(12312)
+})
+
+console.log(p1)
+
+
+foo();
+
+console.log('lly13333333')
