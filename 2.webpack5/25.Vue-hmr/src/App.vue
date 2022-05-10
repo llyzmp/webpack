03b25277c8b5e4940055a433d1c9e3d1913c8432
zmplly <1776321381@qq.com>
2022-05-10 22:05:02 +0800

    feat: vue组件热更新

---
+++ 2.webpack5/25.Vue-hmr/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="example">{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello world! 3332',
+    }
+  },
+}
+</script>
+
+<style>
+.example {
+  color: red;
+}
+</style>
